4e727c3096592a4302ef048f2ab5458746645fe2
Angel Malavar <angel.malavar@gmail.com>
2018-01-20 13:09:18 -0600

    Configuring auth0

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').load();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
 
 const app = express(); // create our app with express
 const morgan = require('morgan'); // log request to the console (express4)
@@ -14,6 +18,30 @@ const config = require('./config');
 
 config.db.connect();
 
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback' 
+  }, (accessToken, refreshToken, extraParams, profile, done) => {
+  // accessToken is the token to call Auth0 API (not needed in the most cases)
+  // extraParams.id_token has the JSON Web Token
+  // profile has all the information from the user
+  return done(null, profile);
+});
+
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 app.use(express.static(`${__dirname}/public`)); // set the static files location /public/img will be /img for use
 app.use(morgan('dev')); // log ecery request to the console
 app.use(bodyParser.urlencoded({
@@ -24,8 +52,17 @@ app.use(bodyParser.json({
   type: 'application/vnd.api+json'
 })); // parse application/vnd.api+json as json
 
-// define model ====================
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(flash());
 
 // routes =========================================
 app.use('/api/user', users);
@@ -34,10 +71,11 @@ app.use('/api/job', jobs);
 
 // application ========================================
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendfile('./public.index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // listen (start app with node server.js) ==========================
-app.listen(5000);
+app.listen(5000);// define model ====================
+
 logger.info('App listening on port 5000');
