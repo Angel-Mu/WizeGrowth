1a84efdc3eb6e4e34f1fc52febf0a04a4d26cc32
Angel Malavar <angel.malavar@gmail.com>
2018-01-20 11:28:11 -0600

    wizers missing props model

--- models/wizers.js
+++ models/wizers.js
@@ -3,27 +3,42 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 
-const starsSchema = {
-  _id: { type: ObjectId, ref: 'stars' },
-  caption: { type: String },
-};
-
 const rolesSchema = {
   _id: { type: ObjectId, ref: 'roles' },
   name: { type: String },
+};
+
+const jobsSchema = {
+  _id: { type: ObjectId, ref: 'jobs' },
+  name: { type: String },
+};
+
+const categoriesSchema = {
+  _id: { type: ObjectId, ref: 'categories' },
+  name: { type: String },
+};
+
+const starsSchema = {
+  _id: { type: ObjectId, ref: 'stars' },
+  caption: { type: String },
+  requirements: { type: String },
+  image: { type: String },
+  rate: { type: String, enum: [ 'BRONZE', 'SILVER', 'GOLD' ] },
+  category: categoriesSchema,
+  given_by: {
+    _id: { type: ObjectId, ref: 'wizers' },
+    fullname: { type: String },
+    additional_notes: { type: String },
+  },
+  received_date: { type: Date },
 };
 
 const wizersSchema = new Schema({
   email: { type: String, required: true, trim: true, unique: true },
   fullname: { type: String },
   role: [ rolesSchema ],
-  caption: { type: String },
-  description: { type: String },
-  requirements: { type: String },
-  rate: { type: String, enum: [ 'BRONZE', 'SILVER', 'GOLD' ] },
   image: { type: String },
-  jobs: [ starsSchema ],
-  category: categorySchema,
+  jobs: [ jobsSchema ],
 });
 
 module.exports = mongoose.model('wizers', wizersSchema);
