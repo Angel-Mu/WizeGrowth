27171cc76e0b381c2f1f9be6670cd9788fe4cb4b
Vicente Neto <vicente.ramos@wizeline.com>
2018-01-20 15:58:28 -0600

    Updating the Wizer collection

--- api/controllers/assign.js
+++ api/controllers/assign.js
@@ -1,52 +1,27 @@
-const bamboohr = require('../services/bamboohr');
-const Reward = require('../models/rewards');
-const Star = require('../models/stars');
-
-function userHasStar(user, star) {
-  return new Promise(function (resolve, reject) {
-    Reward.find({user: user, star: star})
-      .then(function (rewards) {
-        resolve(rewards);
-      })
-      .catch(function (err) {
-        reject(err);
-      });
-  });
-}
+const Wizer = require('../models/wizers');
 
 function assign(req, res) {
-  Star.findById(req.body.star)
-    .then(function (star) {
-      if (star) {
-        bamboohr.getEmployee(req.body.user)
-          .then(function (response) {
-            userHasStar(req.body.user, req.body.star)
-              .then(function (rewards) {
-                if (rewards.length) {
-                  res.status(400).json({message: `User "${response.body.displayName}" already has "${star.caption}" star.`});
-                } else {
-                  Reward.create({user: req.body.user, star: star})
-                    .then(function (reward) {
-                      res.status(201).json(reward);
-                    })
-                    .catch(function (err) {
-                      res.status(500).json({message: err.message});
-                    });
-                }
-              })
-              .catch(function (err) {
-                res.status(500).json({message: err.message});
-              });
+  Wizer.find({email: req.body.wizer.email})
+    .then(function (wizers) {
+      if (wizers.length) {
+        Wizer.findByIdAndUpdate(wizers[0]._id, {$push: {stars: req.body.star}})
+          .then(function (wizer) {
+            res.status(200).json(wizer);
           })
           .catch(function (err) {
-            res.status(err.status).json({message: err.message});
+            res.status(500).json({message: err.message});
           });
       } else {
-        res.status(400).json({message: `Star "${req.body.star}" does not exist.`});
+        let wizer = req.body.wizer;
+        wizer.stars = [req.body.star];
+        Wizer.create(wizer)
+          .then(function (wizer) {
+            res.status(201).json(wizer);
+          })
+          .catch(function (err) {
+            res.status(500).json({message: err.message});
+          });
       }
-    })
-    .catch(function (err) {
-      res.status(500).json({message: err.message});
     });
 }
 
---
+++ api/models/wizers.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const categoriesSchema = new Schema({
+  name: {type: String},
+  description: {type: String}
+});
+
+const starsSchema = new Schema({
+  caption: {type: String},
+  description: {type: String},
+  requirements: {type: String},
+  rate: {type: String, enum: ['BRONZE', 'SILVER', 'GOLD']},
+  image: {type: String},
+  jobs: [Number],
+  category: categoriesSchema,
+  received_date: {type: Date},
+  given_by: {
+    _id: {type: ObjectId, ref: 'wizers'},
+    displayName: {type: String}
+  }
+});
+
+const wizersSchema = new Schema({
+  email: {type: String, required: true, trim: true, unique: true},
+  displayName: {type: String},
+  jobTitle: {type: String},
+  image: {type: String},
+  stars: [starsSchema]
+});
+
+module.exports = mongoose.model('wizers', wizersSchema);
