31e177d1ff7ee2c21db7e08c1b32bb3b85cb13c5
Vicente Neto <vicente.ramos@wizeline.com>
2018-01-20 16:45:41 -0600

    Fixing stars page

--- public/js/stars.js
+++ public/js/stars.js
@@ -1,78 +1,24 @@
 var wizeGrowth = angular.module('wizeGrowth', []);
 
 function mainController($scope, $http) {
+  $http.get('/api/star')
+    .success(function (data) {
+      $scope.stars = data;
+    });
+
 	$scope.saveStar = function() {
+		$scope.formData.jobs = [18265, 18263, 18292, 18316];
 		$http.post('/api/star', $scope.formData)
 		.success(function(data) {
 			$scope.formData = {};
-			$scope.stars = data;
+			$scope.stars.push(data);
+
+			$('#myModal').modal('toggle');
+
 			console.log(data);
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
 		});
 	};
-
-	$scope.stars = [
-		{
-			id:1,
-			name:'The hackathon Winner',
-			desc:'Win a hackathon',
-			prize:'1500 USD',
-		},
-		{
-			id:2,
-			name:'The hackathon Participant',
-			desc:'Participate in a Hackathon',
-			prize:'',
-		},
-		{
-			id:3,
-			name:'The buddy',
-			desc:'Being a buddy',
-			prize:'',
-		},
-		{
-			id:4,
-			name:'The internal lecturer',
-			desc:'Give a lecture inside wizeline',
-			prize:'',
-		},
-		{
-			id:5,
-			name:'The outsider',
-			desc:'Give a lecture outside wizeline',
-			prize:'',
-		},
-		{
-			id:6,
-			name:'',
-			desc:'',
-			prize:'',
-		},
-		{
-			id:7,
-			name:'',
-			desc:'',
-			prize:'',
-		},
-		{
-			id:8,
-			name:'',
-			desc:'',
-			prize:'',
-		},
-		{
-			id:9,
-			name:'',
-			desc:'',
-			prize:'',
-		},
-		{
-			id:10,
-			name:'',
-			desc:'',
-			prize:'',
-		}
-	];
 }
